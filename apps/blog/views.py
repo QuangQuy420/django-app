ba43d12b9810aa67b97747d7f5355d7f9397ba91
Quy Phan Quang <phanquangquy9a5@gmail.com>
2025-11-26 17:24:56 +0700

    feat: create login/logout and post ui

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,12 +1,29 @@
-from rest_framework import generics, permissions
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from .models import Post
-from .serializers import PostSerializer
 
-class PostListCreateView(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    # default permission IsAuthenticated from settings, so can omit
-    permission_classes = [permissions.IsAuthenticated]
+# ---- HTML views ----
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ["-created_at"]
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy("blog:post_list")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from apps.blog.models import Post
+
+User = get_user_model()
+
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Post)
