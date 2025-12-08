98a71331cb29d60f5abfc3a68d4b6f4117efb833
Quy Phan Quang <phanquangquy9a5@gmail.com>
2025-12-08 15:45:55 +0700

    feat: add dockerfile and adjust docs for setup

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-eu*6)zs+9c)(aq*8x1o&!bw-g94rq0bkn7c%scbbzr)89q6h7)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
